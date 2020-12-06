2412ad59bf21bc3866670cae5ea98b03914041b0
programandala.net <github@programandala.net>
2020-12-06 22:49:22 +0000

    Convert the Z80 assembler code to Forth.
    FossilOrigin-Name: 92eb86d26a0a613ff4bb4976bd9d348a888e6cdddda2b81c3a573ad8df634def

--- src/data_stack.fs
+++ src/data_stack.fs
@@ -9,114 +9,112 @@
 
 \ By Marcos Cruz (programandala.net) 2015, 2016, 2020.
 
-\ Last modified: 202012062211.
+\ Last modified: 202012062344.
 \ See change log at the end of the file.
 
 \ ==============================================================
 \ Description {{{1
 
-\ This file provides the Z80 code that manages the data stack. It is
-\ adapted from Couplement Forth v0.2.0-dev.30.0+202012062211
+\ This file provides the Z80 code that manages the data stack.
+
+\ ==============================================================
+\ License
+
+\ Copyright (C) 2015,2016,2020 Marcos Cruz (programandala.net)
+
+\ You may do whatever you want with this work, so long as you
+\ retain all copyright notices, all credit notices, and this
+\ license in all files of all redistributed copies and derived
+\ works. There is no warranty.
+
+\ ==============================================================
+\ Credits
+
+\ This code was adapted from Couplement Forth
+\ v0.2.0-dev.30.0+202012062211
 \ (http://programandala.net/en.program.couplement_forth).
 
-0 [if] \ XXX TODO Adapt the Z80 assembler code to Forth.
+\ Couplement Forth was forked from ForthCoupe
+\ (http://programandala.net/en.program.forthcoupe.html), an abandoned
+\ Forth for the SAM Coupé, based on John A. Avis' SamForth
+\ (http://programandala.net/en.program.samforth.html).
 
-; --------------------------------------------------------------
-; License
+\ ==============================================================
 
-; Copyright (C) 2015,2016,2020 Marcos Cruz (programandala.net)
+$0100 memory +!
+memory> @ constant sp0
+  \ The data stack grows from bottom (high memory) to top (low memory)
 
-; You may do whatever you want with this work, so long as you
-; retain all copyright notices, all credit notices, and this
-; license in all files of all redistributed copies and derived
-; works. There is no warranty.
+: init-data-stack ( -- )
+  sp0 ix ldp#,
+  ret, ;
 
-; --------------------------------------------------------------
-; Credits
+: push-de ( x -- )
+                      \ T Cycles
+  ix decp,            \  10
+  d 0 ix stx,         \  19
+  ix decp,            \  10
+  e 0 ix stx,         \  19
+  ret, ;              \  10
+                      \ 068 total (68 average with pop-de)
 
-; Couplement Forth was forked from ForthCoupe
-; (http://programandala.net/en.program.forthcoupe.html), an
-; abandoned Forth for the SAM Coupé, based on John A. Avis'
-; SamForth (http://programandala.net/en.program.samforth.html).
+: pop-de ( -- x )
+                      \ T Cycles
+  0 ix e ftx,         \  19
+  ix incp,            \  10
+  0 ix d ftx,         \  19
+  ix incp,            \  10
+  ret, ;              \  10
+                      \ 068 total (68 average with push-de)
 
+: push-hl ( x -- )
+                      \ T Cycles
+  ix decp,            \  10
+  h 0 ix stx,         \  19
+  ix decp,            \  10
+  l 0 ix stx,         \  19
+  ret, ;              \  10
+                      \ 068 total (68 average with pop-hl)
 
-data_stack_limit:
-data_stack_max_top:
-  ; The data stack grows from bottom (high memory) to top (low memory)
-  defs $0100
-data_stack_bottom:
+: pop-hl ( -- x )
+                      \ T Cycles
+  0 ix l ftx,         \  19
+  ix incp,            \  10
+  0 ix h ftx,         \  19
+  ix incp,            \  10
+  ret, ;              \  10
+                      \ 068 total (68 average with push-hl)
 
-init_data_stack:
-  ld ix,(sp0)
-  ret
+: push-de-hl ( d -- )
+                      \ T Cycles
+  ix decp,            \  10
+  d 0 ix stx,         \  19
+  ix decp,            \  10
+  e 0 ix stx,         \  19
+  ix decp,            \  10
+  h 0 ix stx,         \  19
+  ix decp,            \  10
+  l 0 ix stx,         \  19
+  ret, ;              \  10
+                      \ 126 total (126 average with pop-de-hl)
 
-push_de:
-                      ; T Cycles
-  dec ix              ;  10
-  ld (ix),d           ;  19
-  dec ix              ;  10
-  ld (ix),e           ;  19
-  ret                 ;  10
-                      ; 068 total (68 average with pop_de)
-
-pop_de:
-                      ; T Cycles
-  ld e,(ix)           ;  19
-  inc ix              ;  10
-  ld d,(ix)           ;  19
-  inc ix              ;  10
-  ret                 ;  10
-                      ; 068 total (68 average with push_de)
-
-push_hl:
-                      ; T Cycles
-  dec ix              ;  10
-  ld (ix),h           ;  19
-  dec ix              ;  10
-  ld (ix),l           ;  19
-  ret                 ;  10
-                      ; 068 total (68 average with pop_hl)
-
-pop_hl:
-                      ; T Cycles
-  ld l,(ix)           ;  19
-  inc ix              ;  10
-  ld h,(ix)           ;  19
-  inc ix              ;  10
-  ret                 ;  10
-                      ; 068 total (68 average with push_hl)
-
-push_de_hl:
-                      ; T Cycles
-  dec ix              ;  10
-  ld (ix),d           ;  19
-  dec ix              ;  10
-  ld (ix),e           ;  19
-  dec ix              ;  10
-  ld (ix),h           ;  19
-  dec ix              ;  10
-  ld (ix),l           ;  19
-  ret                 ;  10
-                      ; 126 total (126 average with pop_de_hl)
-
-pop_hl_de:
-                      ; T Cycles
-  ld l,(ix)           ;  19
-  inc ix              ;  10
-  ld h,(ix)           ;  19
-  inc ix              ;  10
-  ld e,(ix)           ;  19
-  inc ix              ;  10
-  ld d,(ix)           ;  19
-  inc ix              ;  10
-  ret                 ;  10
-                      ; 126 total (126 average with push_hl_de)
-
-[then]
+: pop-hl-de ( -- d )
+                      \ T Cycles
+  0 ix l ftx,         \  19
+  ix incp,            \  10
+  0 ix h ftx,         \  19
+  ix incp,            \  10
+  0 ix e ftx,         \  19
+  ix incp,            \  10
+  0 ix d ftx,         \  19
+  ix incp,            \  10
+  ret, ;              \  10
+                      \ 126 total (126 average with push-hl-de)
 
 \ ==============================================================
 \ Change log {{{1
 
-\ 2020-12-06: Code copied from Couplement Forth
+\ 2020-12-06: Copy the Z80 assembler code from Couplement Forth
 \ v0.2.0-dev.30.0+202012062153
-\ (http://programandala.net/en.program.couplement_forth).
+\ (http://programandala.net/en.program.couplement_forth) and adapt it
+\ to Forth.
