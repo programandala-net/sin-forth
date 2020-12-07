ebbca7302a827359dc973c91c58d610227b6d216
programandala.net <github@programandala.net>
2020-12-07 21:56:30 +0000

    Add `emit`.
    FossilOrigin-Name: 8277f0cc949e06fdf1c943f72364a94adcb2fcefb2e8803df69a7689b0cb823d

---
+++ src/lib/emit.fs
@@ -0,0 +1,49 @@
+\ lib/emit.fs
+
+\ This file is part of Sin Forth.
+
+\ This file adds `emit` to the compiler.
+
+\ By Marcos Cruz (programandala.net), 2020.
+
+\ Last modified: 202012072255.
+\ See change log at the end of the file.
+
+\ ==============================================================
+
+get-order get-current
+
+require pop-hl.fs \ `pop-hl`
+
+target-definitions
+
+: emit ( c -- )
+  pop-hl
+  l a ld,       \ ld a,l
+: emit-a
+  $10 rst,      \ rst $10
+  $FF a ld#,    \ ld a,$FF
+  #23692 sta, ; \ ld(sys_scr_ct),a ; no "scroll?" message
+
+  \ doc{
+  \
+  \ emit ( c -- )
+  \
+  \ Display character _c_.
+  \
+  \ }doc
+
+  \ doc{
+  \
+  \ emit-a ( -- )
+  \
+  \ Display the character contained in the ``A`` Z80 register.
+  \
+  \ }doc
+
+set-current set-order
+
+\ ==============================================================
+\ Change log {{{1
+
+\ 2020-12-07: Start.
