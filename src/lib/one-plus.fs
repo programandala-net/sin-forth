72ce82e91980c0ace59be28f9553588c73af52ff
programandala.net <github@programandala.net>
2020-12-15 16:02:11 +0000

    Write faster and smaller `1+`.
    FossilOrigin-Name: 3049f176f16eaab532ddb1241970743dc884164540f1af626d19ecf41ef3e9c1

--- src/lib/one-plus.fs
+++ src/lib/one-plus.fs
@@ -6,7 +6,7 @@
 
 \ By Marcos Cruz (programandala.net), 2020.
 
-\ Last modified: 202012120005.
+\ Last modified: 202012151700.
 \ See change log at the end of the file.
 
 \ ==============================================================
@@ -18,14 +18,20 @@ target-definitions
 : 1+ ( n1 -- n2 )
 
   \ XXX slow variant:
-  \ pop-hl h incp, push-hl ;
+  \ pop-hl h incp, push-hl
 
   \ XXX fast variant:
-  0 ix l ftx, \ ld l,(ix+0)
-  1 ix h ftx, \ ld h,(ix+1)
-  h incp,     \ inc hl
-  l 0 ix stx, \ ld (ix+0),l
-  h 1 ix stx, \ ld (ix+1),h
+  \ 0 ix l ftx, \ ld l,(ix+0)
+  \ 1 ix h ftx, \ ld h,(ix+1)
+  \ h incp,     \ inc hl
+  \ l 0 ix stx, \ ld (ix+0),l
+  \ h 1 ix stx, \ ld (ix+1),h
+
+  \ XXX even faster variant:
+  0 ix incx,  \ inc (ix+0) ; low byte
+  nz? ?ret,
+  1 ix incx,  \ inc (ix+1) ; high byte
+
   ;           \ ret
 
 set-current set-order
@@ -34,3 +40,5 @@ set-current set-order
 \ Change log {{{1
 
 \ 2020-12-11: Start.
+\
+\ 2020-12-15: Write faster variant with `incx,`.
