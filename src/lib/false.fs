13c37d7b6c08939e3ad8049898f31a8b0daa96ac
programandala.net <github@programandala.net>
2020-12-18 23:21:57 +0000

    Add `true` and `false`.
    FossilOrigin-Name: 3047da74988db6d97b9f5f2a987788f653d656ebd767c124fc60b1714a1a1d20

---
+++ src/lib/false.fs
@@ -0,0 +1,41 @@
+\ lib/false.fs
+
+\ This file is part of Sin Forth.
+
+\ This file adds `false` to the target system.
+
+\ By Marcos Cruz (programandala.net), 2020.
+
+\ Last modified: 202012122003.
+\ See change log at the end of the file.
+
+\ ==============================================================
+
+require push-hl.fs \ `push-hl`
+
+get-order get-current
+
+target-definitions
+
+: false ( -- false )
+  $0000 h ldp#, \ ld hl,$0000
+  push-hl ;
+
+  \ doc{
+  \
+  \ false ( -- false )
+  \
+  \ Return a false flag, a single-cell value with all its bits clear
+  \ (equivalent to zero).
+  \
+  \ See also: `false`.
+  \
+  \ }doc
+
+set-current set-order
+
+\ ==============================================================
+\ Change log {{{1
+
+\ 2020-12-12: Start.
+
---
+++ src/lib/true.fs
@@ -0,0 +1,40 @@
+\ lib/true.fs
+
+\ This file is part of Sin Forth.
+
+\ This file adds `true` to the target system.
+
+\ By Marcos Cruz (programandala.net), 2020.
+
+\ Last modified: 202012122003.
+\ See change log at the end of the file.
+
+\ ==============================================================
+
+require push-hl.fs \ `push-hl`
+
+get-order get-current
+
+target-definitions
+
+: true ( -- true )
+  $FFFF h ldp#, \ ld hl,$FFFF
+  push-hl ;
+
+  \ doc{
+  \
+  \ true ( -- true )
+  \
+  \ Return a true flag, a single-cell value with all its bits
+  \ set (equivalent to `-1`).
+  \
+  \ See also: `false`.
+  \
+  \ }doc
+
+set-current set-order
+
+\ ==============================================================
+\ Change log {{{1
+
+\ 2020-12-12: Start.
