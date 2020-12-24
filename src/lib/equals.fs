bd432e06c4b6f5339a08937af9b0a62c8eb79698
programandala.net <github@programandala.net>
2020-12-24 00:54:02 +0000

    Add `=`, already used by <test_06_flow.fs>.
    FossilOrigin-Name: 04505e149dc67daf4f08403bb13ee0de42168df29203393295dde47c36093050

---
+++ src/lib/equals.fs
@@ -0,0 +1,46 @@
+\ lib/equals.fs
+
+\ This file is part of Sin Forth.
+
+\ This file adds `=` to the target system.
+
+\ By Marcos Cruz (programandala.net), 2020.
+
+\ Last modified: 202012122003.
+\ See change log at the end of the file.
+
+\ ==============================================================
+
+require false.fs     \ `false`
+require pop-de-hl.fs \ `pop-de-hl`
+require true.fs      \ `true`
+
+get-order get-current
+
+target-definitions
+
+: = ( x1 x2 -- f )
+  pop-de-hl          \ pop de
+                     \ pop hl
+  a or,              \ or a ; Fc=0
+  d sbcp,            \ sbc hl,de
+  false_dea nz? ?jp, \ jp nz,false_dea
+  true_dea jp,       \ jp true_dea
+
+  \ doc{
+  \
+  \ = ( x1 x2 -- f ) "equals"
+  \
+  \ _f_ is true only and only if _x1_ is bit-for-bit the same as
+  \ _x1_.
+  \
+  \ See also: `<>`, `>`, `<`.
+  \
+  \ }doc
+
+set-current set-order
+
+\ ==============================================================
+\ Change log {{{1
+
+\ 2020-12-12: Start.
