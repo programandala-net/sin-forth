06d6f132cb726b25210cf7d5dc5250e1a64398d1
Marcos Cruz <mercurial-repo@programandala.net>
2023-04-24 16:12:10 +0200

    lib: activate and fix `hl<de?` and `<`

--- src/lib/less-than.fs
+++ src/lib/less-than.fs
@@ -1,6 +1,6 @@
 \ lib/less-than.fs
 \ by Marcos Cruz (programandala.net), 2020, 2023.
-\ Last modified: 20230423T1138+0200.
+\ Last modified: 20230423T1723+0200.
 
 \ This file is part of Sin Forth
 \ by Marcos Cruz (programandala.net), 2010/2023.
@@ -26,24 +26,21 @@ target-definitions
 
 : hl<de? ( -- f )
 
-  assembler{
-
-  \ a or,               \ or a      ; Fc=0
-  \ d sbcp,             \ sbc hl,de ; n1-n2 in HL, SZVC valid
-  \                     \ ; negative & not OV:  HL<DE
-  \                     \ ; negative & OV:      HL +ve, DE -ve, result -ve, so HL>DE
-  \                     \ ; positive & not OV:  HL>=DE
-  \                     \ ; positive & OV:      HL -ve, DE +ve, result +ve, so HL<DE
-  \                     \ ; thus OV reverses the sense of the sign bit
-  \ po? aif             \ jp pe,less_than.reverse  ; if OV, jump
-  \                     \ ; not overflow
-  \   false_dea p? ?jp, \ jp p,false_    ; if +ve, result false
-  \   true_dea jp,      \ jp true_       ; if -ve, result true
-  \ athen               \ less_than.reverse:
-  \                     \ ; overflow
-  \ false_dea m? ?jp,   \ jp m,false_    ; if -ve, result false
-  \ true_dea jp,        \ jp true_       ; if +ve, result true
-  }
+  a or,               \ or a      ; Fc=0
+  d sbcp,             \ sbc hl,de ; n1-n2 in HL, SZVC valid
+                      \ ; negative & not OV:  HL<DE
+                      \ ; negative & OV:      HL +ve, DE -ve, result -ve, so HL>DE
+                      \ ; positive & not OV:  HL>=DE
+                      \ ; positive & OV:      HL -ve, DE +ve, result +ve, so HL<DE
+                      \ ; thus OV reverses the sense of the sign bit
+  pe? aif             \ jp pe,less_than.reverse  ; if OV, jump
+                      \ ; not overflow
+    false_dea p? ?jp, \ jp p,false_    ; if +ve, result false
+    true_dea jp,      \ jp true_       ; if -ve, result true
+  athen               \ less_than.reverse:
+                      \ ; overflow
+  false_dea m? ?jp,   \ jp m,false_    ; if -ve, result false
+  true_dea jp,        \ jp true_       ; if +ve, result true
 
   \ Credit: Code adapted from Solo Forth, previously adapted from Z88
   \ CamelForth (Gary Lancaster, 2001).
