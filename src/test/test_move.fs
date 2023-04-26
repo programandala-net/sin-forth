f511a0fead7020dee0b3e93ced79d51c4359f6a1
Marcos Cruz <mercurial-repo@programandala.net>
2023-04-26 07:24:01 +0200

    test: fix "move"

--- src/test/test_move.fs
+++ src/test/test_move.fs
@@ -1,6 +1,6 @@
 \ test_move.fs
 \ by Marcos Cruz (programandala.net), 2020, 2023.
-\ Last modified: 20230423T1138+0200.
+\ Last modified: 20230426T0720+0200.
 
 \ This file is part of Sin Forth
 \ by Marcos Cruz (programandala.net), 2010/2023.
@@ -13,8 +13,6 @@
 \ license.  There is no warranty.
 
 \ ==============================================================
-
-\ XXX FIXME crash
 
 40000 set-origin
 
@@ -31,41 +29,46 @@ require sin_forth/lib/pop-hl.fs
 require sin_forth/lib/r-fetch.fs
 require sin_forth/lib/step.fs
 
-22528 h-constant attr
-768 h-constant /attr
+22528 h-constant attr   \ display attributes address
+  768 h-constant /attr  \ display attributes size in bytes
 
-compiler{ memory> } h-@ h-constant attr-copy
+t-here h-constant attr-copy
 /attr t-allot
 
 : save-attr ( -- )
-  attr h ldp#,
-  attr-copy d ldp#,
-  /attr b ldp#,
-  move-hl-de-bc ;
+  attr h ldp#,      \ ld hl, attr
+  attr-copy d ldp#, \ ld de, attr_copy
+  /attr b ldp#,     \ ld bc, attr_len
+  move-hl-de-bc ;   \ jp move_hl_de_bc
+  \ Move `/attr` bytes from `attr`, the display attributes address, to
+  \ the `attr-copy` buffer.
 
 : restore-attr ( -- )
-  attr-copy h ldp,
+  attr-copy h ldp#,  \ ld hl, attr_copy
+  \ Move `/attr` bytes from the `attr-copy` buffer to `attr`, the
+  \ display attributes address.
+
 : hl-to-attr ( -- )
-  attr d ldp#,
-  /attr b ldp#,
-  move-hl-de-bc ;
+  attr d ldp#,    \ ld de, attr
+  /attr b ldp#,   \ ld bc, attr_len
+  move-hl-de-bc ; \ jp move_hl_de_bc
+  \ Move `/attr` bytes from the address pointed by the `hl` register
+  \ to `attr`, the display attributes address.
 
 : wait ( -- )
-  65535 literal for step ;
+  255 literal for step ;
+  \ Execute a noop loop 256 times (255..0).
 
 : rubbish ( u -- )
-  for r@ pop-hl hl-to-attr step ;
+  for r@ pop-hl hl-to-attr wait step ;
+  \ Copy _u+1_ times `/attr` bytes from the address pointed by the
+  \ loop index (_u..0_) to `attr`, the display attributes address.
 
 boot-here
 exx, h push,
 
-\ 0 h ldp#,
-\ attr d ldp#,
-\ /attr b ldp#,
-\ move-hl-de-bc
-
 save-attr
-768 literal rubbish
+255 literal rubbish
 restore-attr
 
 h pop, exx,
