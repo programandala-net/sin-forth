c30e9ffd58ec8a79d2732ffc5c709b7c6d98590d
Marcos Cruz <mercurial-repo@programandala.net>
2023-05-05 10:19:41 +0200

    lib: restore `allot`
    The code was removed by mistake in changeset 329:293bc1fb9d96.

--- src/lib/allot.fs
+++ src/lib/allot.fs
@@ -1,6 +1,6 @@
 \ lib/allot.fs
 \ by Marcos Cruz (programandala.net), 2020, 2023.
-\ Last modified: 20230501T1150+0200.
+\ Last modified: 20230505T1017+0200.
 
 \ This file is part of Sin Forth
 \ by Marcos Cruz (programandala.net), 2010/2023.
@@ -16,6 +16,8 @@
 
 require d-p.fs        \ `dp`
 require plus-store.fs \ `+!`
+
+: allot ( n -- ) dp +! ;
 
   \ doc{
   \
